af15dfeee95c225a0d2cf91c67b0216abd914d1f
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-02-24 20:52:00 +0530

    Added features

--- src/components/product/Carousel.jsx
+++ src/components/product/Carousel.jsx
@@ -82,69 +82,67 @@ const Collection = () => {
           data-slider-container
         >
           {slidesOnDisplay.map((item) => (
-            <Link to={`/explore/id${item.key}`}>
-              <li className="slider-item" key={item.key} onClick={displayItem}>
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    style={{ width: "500", height: "500" }}
+            <li className="slider-item" key={item.key} onClick={displayItem}>
+              <div className="collection-card card">
+                <figure
+                  className="card-banner img-holder"
+                  style={{ width: "500", height: "500" }}
+                >
+                  <img
+                    src={item.imgSrc}
+                    width="500"
+                    height="500"
+                    loading="lazy"
+                    alt="Digital Collection"
+                    className="img-cover"
+                  ></img>
+                </figure>
+
+                <div className="card-content">
+                  <div
+                    className="card-profile"
+                    style={{
+                      display: "flex",
+                    }}
                   >
                     <img
-                      src={item.imgSrc}
-                      width="500"
-                      height="500"
+                      src={item.avatarSrc}
+                      width="64"
+                      height="64"
                       loading="lazy"
-                      alt="Digital Collection"
-                      className="img-cover"
+                      alt="CutieGirl profile"
                     ></img>
-                  </figure>
 
-                  <div className="card-content">
-                    <div
-                      className="card-profile"
+                    <FontAwesomeIcon
+                      icon={faCheckCircle}
+                      name="checkmark-circle"
+                      aria-hidden="true"
                       style={{
-                        display: "flex",
+                        marginTop: "auto",
+                        position: "relative",
+                        left: "-10px",
+                        color: "#059467",
                       }}
-                    >
-                      <img
-                        src={item.avatarSrc}
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="CutieGirl profile"
-                      ></img>
-
-                      <FontAwesomeIcon
-                        icon={faCheckCircle}
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                        style={{
-                          marginTop: "auto",
-                          position: "relative",
-                          left: "-10px",
-                          color: "#059467",
-                        }}
-                      ></FontAwesomeIcon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <Link to="/" className="link:hover">
-                        {item.title}
-                      </Link>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <Link to="/" className="link">
-                        {item.name}
-                      </Link>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
+                    ></FontAwesomeIcon>
                   </div>
+
+                  <h3 className="title-md card-title">
+                    <Link to="/" className="link:hover">
+                      {item.title}
+                    </Link>
+                  </h3>
+
+                  <p className="label-md card-author">
+                    by{" "}
+                    <Link to="/" className="link">
+                      {item.name}
+                    </Link>
+                  </p>
+
+                  <p className="card-text">25 Items</p>
                 </div>
-              </li>
-            </Link>
+              </div>
+            </li>
           ))}
         </ul>
 
